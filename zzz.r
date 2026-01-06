04b9e267009bd98ad52e5ccf1cb3ecf657474386
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2026-01-06 11:10:58 -1000

    add renv

---
+++ zzz.r
@@ -0,0 +1,8 @@
+# libraries that are needed but aren't referenced in the code
+# below libraries are only needed for VSCode-R functionality
+library(rlang)
+library(jsonlite)
+library(httpgd)
+library(languageserver)
+library(gitcreds)
+library(rsconnect)
