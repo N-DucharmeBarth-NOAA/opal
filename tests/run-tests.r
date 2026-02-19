ecaa9458b0bb82c1279a179351c18c2fe6f09479
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2026-02-19 08:17:47 -1000

    opal: initial restructure as bare bones R package

--- tests/run-tests.r
+++
@@ -1,7 +0,0 @@
-# Run all tests for biomass extraction functions
-# Usage: source this file from R console in the project directory
-
-library(testthat)
-
-# Set project directory (assumes this script is in tests/)
-test_dir("tests/testthat", reporter = "progress")
--- zzz.r
+++
@@ -1,9 +0,0 @@
-# libraries that are needed but aren't referenced in the code
-# below libraries are only needed for VSCode-R functionality
-library(rlang)
-library(jsonlite)
-library(httpgd)
-library(languageserver)
-library(gitcreds)
-library(rsconnect)
-library(RTMB)
