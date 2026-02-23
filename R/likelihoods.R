a2be444977ae44ddecbf889853616c66463b0f5e
Darcy Webber <darcy@quantifish.co.nz>
2026-02-23 13:57:25 +1300

    annother go

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -63,9 +63,135 @@ get_cpue_like <- function(data, parameters, number_ysa, sel_fya, creep_init = 1)
 #' @importFrom RTMBdist ddirichlet ddirmult
 #' @export
 #' 
+# get_length_like <- function(data, parameters, catch_pred_fya, pla) {
+#   "[<-" <- ADoverload("[<-")
+#   "c" <- ADoverload("c")
+#   getAll(data, parameters, warn = FALSE)
+#   
+#   n_f <- length(lf_n_f)
+#   lf_year_fi <- split(lf_year, lf_fishery)
+#   lf_n_fi <- split(lf_n, lf_fishery)
+#   n_lf <- nrow(lf_obs_in)
+#   n_len_local <- ncol(lf_obs_in)
+#   # lp <- numeric(n_lf)
+#   # lf_pred <- matrix(0, n_lf, n_len_local)
+#   lp <- lf_pred <- vector("list", n_f)
+#   
+#   # ---- Build lf_pred on the AD tape (gradients flow through pla & catch_pred_fya) ----
+#   for (j in seq_len(n_f)) {
+#     f <- lf_fishery_f[j]
+#     bmin <- lf_minbin[f]
+#     bmax <- lf_maxbin[f]
+#     lf_pred[[j]] <- matrix(0, lf_n_f[j], bmax - bmin + 1)
+#     for (i in seq_len(lf_n_f[j])) {
+#       y <- lf_year_fi[[j]][i]
+#       catch_a <- catch_pred_fya[f, y, ]
+#       pred <- c(pla %*% catch_a)
+#       if (bmin > 1) pred[bmin] <- sum(pred[1:bmin])
+#       if (bmax < n_len_local) pred[bmax] <- sum(pred[bmax:n_len_local])
+#       pred <- pred[bmin:bmax]
+#       pred <- pred + 1e-8
+#       pred <- pred / sum(pred)
+#       lf_pred[[j]][i, ] <- pred
+#     }
+#   }
+#   
+#   # for (j in seq_len(n_f)) {
+#   #   f <- lf_fishery_f[j]
+#   #   # Handle bin aggregation
+#   #   bmin <- lf_minbin[f]
+#   #   bmax <- lf_maxbin[f]
+#   #   # lf_obs[[f]] <- matrix(0, lf_n_f[j], bmax - bmin + 1)
+#   #   lf_obs[[j]] <- lf_pred[[j]] <- matrix(0, lf_n_f[j], bmax - bmin + 1)
+#   #   # f <- lf_fishery[i]
+#   #   for (i in seq_len(lf_n_f[j])) {
+#   #     y <- lf_year_fi[[j]][i]
+#   #     # Predicted catch-at-age for this fishery and timestep
+#   #     catch_a <- catch_pred_fya[f, y, ]
+#   #     # Convert to predicted numbers-at-length via PLA
+#   #     # pla is [n_len, n_age], catch_a is [n_age] -> result is [n_len]
+#   #     pred <- c(pla %*% catch_a)
+#   #     # Observed proportions for this row
+#   #     obs <- lf_obs_in[i, ]
+#   #     # Lower-tail aggregation: collapse bins 1:bmin into bin bmin
+#   #     if (bmin > 1) {
+#   #       pred[bmin] <- sum(pred[1:bmin])
+#   #       obs[bmin] <- sum(obs[1:bmin])
+#   #     }
+#   #     # Upper-tail aggregation: collapse bins bmax:n_len into bin bmax
+#   #     if (bmax < n_len_local) {
+#   #       pred[bmax] <- sum(pred[bmax:n_len_local])
+#   #       obs[bmax] <- sum(obs[bmax:n_len_local])
+#   #     }
+#   #     # Subset to active bins
+#   #     pred <- pred[bmin:bmax]
+#   #     obs <- obs[bmin:bmax]
+#   #     # Normalize predicted to proportions (epsilon for AD safety)
+#   #     pred <- pred + 1e-8
+#   #     pred <- pred / sum(pred)
+#   #     lf_obs[[j]][i, ] <- obs * lf_n_fi[[j]][i]# * exp(log_lf_tau[f])
+#   #     lf_pred[[j]][i, ] <- pred
+#   #     # if (lf_switch == 1) lf_obs[i, bmin:bmax] <- obs * lf_n[i] * exp(log_lf_tau[f])
+#   #     # lf_pred[i, bmin:bmax] <- pred # Store predicted proportions for diagnostics
+#   #   }
+#   # }
+#   
+#   # lf_obs <- OBS(lf_obs)
+#   
+#   # Evaluate likelihood
+#   
+#   # for (i in seq_len(n_lf)) {
+#   for (j in seq_len(n_f)) {
+#     f <- lf_fishery_f[j]
+#     bmin <- lf_minbin[f]
+#     bmax <- lf_maxbin[f]
+#     lp[[j]] <- numeric(lf_n_f[j])
+#     for (i in seq_len(lf_n_f[j])) {
+#       if (removal_switch_f[f] == 0 & lf_n_fi[[j]][i] > 0) {
+#         if (lf_switch == 1) { # Multinomial
+#           # obs <- obs * lf_n[i] * exp(log_lf_tau[f])
+#           # lp[i] <- -RTMB::dmultinom(x = obs, prob = pred, log = TRUE) # REMOVE LATER, if you source this files then reverts to stats::dmultinom which return zero as log like
+#           # lp[[j]][i] <- -RTMB::dmultinom(x = lf_obs[[j]][i, ], prob = lf_pred[[j]][i, ], log = TRUE) # REMOVE LATER, if you source this files then reverts to stats::dmultinom which return zero as log like
+#           obs_i <- as.numeric(data$lf_obs[[j]][i, ])
+#           obs_i <- OBS(obs_i)
+#           lp[[j]][i] <- -RTMB::dmultinom(x = obs_i, prob = lf_pred[[j]][i, ], log = TRUE)
+#         }
+#         # if (lf_switch == 2) { # Dirichlet
+#         #   obs <- obs + 1e-8
+#         #   obs <- obs / sum(obs)
+#         #   pred <- pred + 1e-8
+#         #   pred <- pred / sum(pred)
+#         #   lf_alpha <- pred * lf_n[i] * exp(log_lf_tau[f])
+#         #   lp[i] <- -ddirichlet(x = obs, alpha = lf_alpha, log = TRUE)
+#         # }
+#         # if (lf_switch == 3) { # Dirichlet-multinomial
+#         #   obs <- obs * lf_n[i]
+#         #   lf_alpha <- pred * exp(log_lf_tau[f])
+#         #   lp[i] <- -ddirmult(x = obs, size = lf_n[i], alpha = lf_alpha, log = TRUE)
+#         # }
+#         # if (lf_switch == 9) { # Multinomial offset form
+#         #   #   -n_eff * sum(obs * log(pred)) + n_eff * sum(obs * log(obs))
+#         #   # Equivalent to n_eff * KL(obs || pred); minimum NLL = 0 when pred == obs.
+#         #   # Small constant added to both terms for numerical safety and exact
+#         #   # cancellation at perfect fit.
+#         #   n_eff <- lf_n[i] * exp(log_lf_tau[f])
+#         #   lp[i] <- -n_eff * sum(obs * log(pred + 1e-8))
+#         #   lp[i] <- lp[i] + n_eff * sum(obs * log(obs + 1e-8))
+#       }
+#     }
+#   }
+#   
+#   REPORT(lf_pred)
+#   return(unlist(lp))
+# }
+
 get_length_like <- function(data, parameters, catch_pred_fya, pla) {
   "[<-" <- ADoverload("[<-")
   "c" <- ADoverload("c")
+  
+  # Mark the flat obs vector with OBS before getAll
+  data$lf_obs_vec <- OBS(data$lf_obs_vec)
+  
   getAll(data, parameters, warn = FALSE)
   
   n_f <- length(lf_n_f)
@@ -73,11 +199,9 @@ get_length_like <- function(data, parameters, catch_pred_fya, pla) {
   lf_n_fi <- split(lf_n, lf_fishery)
   n_lf <- nrow(lf_obs_in)
   n_len_local <- ncol(lf_obs_in)
-  # lp <- numeric(n_lf)
-  # lf_pred <- matrix(0, n_lf, n_len_local)
   lp <- lf_pred <- vector("list", n_f)
   
-  # ---- Build lf_pred on the AD tape (gradients flow through pla & catch_pred_fya) ----
+  # ---- Build lf_pred on the AD tape ----
   for (j in seq_len(n_f)) {
     f <- lf_fishery_f[j]
     bmin <- lf_minbin[f]
@@ -96,91 +220,25 @@ get_length_like <- function(data, parameters, catch_pred_fya, pla) {
     }
   }
   
-  # for (j in seq_len(n_f)) {
-  #   f <- lf_fishery_f[j]
-  #   # Handle bin aggregation
-  #   bmin <- lf_minbin[f]
-  #   bmax <- lf_maxbin[f]
-  #   # lf_obs[[f]] <- matrix(0, lf_n_f[j], bmax - bmin + 1)
-  #   lf_obs[[j]] <- lf_pred[[j]] <- matrix(0, lf_n_f[j], bmax - bmin + 1)
-  #   # f <- lf_fishery[i]
-  #   for (i in seq_len(lf_n_f[j])) {
-  #     y <- lf_year_fi[[j]][i]
-  #     # Predicted catch-at-age for this fishery and timestep
-  #     catch_a <- catch_pred_fya[f, y, ]
-  #     # Convert to predicted numbers-at-length via PLA
-  #     # pla is [n_len, n_age], catch_a is [n_age] -> result is [n_len]
-  #     pred <- c(pla %*% catch_a)
-  #     # Observed proportions for this row
-  #     obs <- lf_obs_in[i, ]
-  #     # Lower-tail aggregation: collapse bins 1:bmin into bin bmin
-  #     if (bmin > 1) {
-  #       pred[bmin] <- sum(pred[1:bmin])
-  #       obs[bmin] <- sum(obs[1:bmin])
-  #     }
-  #     # Upper-tail aggregation: collapse bins bmax:n_len into bin bmax
-  #     if (bmax < n_len_local) {
-  #       pred[bmax] <- sum(pred[bmax:n_len_local])
-  #       obs[bmax] <- sum(obs[bmax:n_len_local])
-  #     }
-  #     # Subset to active bins
-  #     pred <- pred[bmin:bmax]
-  #     obs <- obs[bmin:bmax]
-  #     # Normalize predicted to proportions (epsilon for AD safety)
-  #     pred <- pred + 1e-8
-  #     pred <- pred / sum(pred)
-  #     lf_obs[[j]][i, ] <- obs * lf_n_fi[[j]][i]# * exp(log_lf_tau[f])
-  #     lf_pred[[j]][i, ] <- pred
-  #     # if (lf_switch == 1) lf_obs[i, bmin:bmax] <- obs * lf_n[i] * exp(log_lf_tau[f])
-  #     # lf_pred[i, bmin:bmax] <- pred # Store predicted proportions for diagnostics
-  #   }
-  # }
-  
-  # lf_obs <- OBS(lf_obs)
-  
-  # Evaluate likelihood
-  
-  # for (i in seq_len(n_lf)) {
+  # ---- Evaluate likelihood ----
+  # Index into the flat OBS vector
+  idx <- 0L
   for (j in seq_len(n_f)) {
     f <- lf_fishery_f[j]
-    bmin <- lf_minbin[f]
-    bmax <- lf_maxbin[f]
+    nbins <- lf_nbins_f[j]
     lp[[j]] <- numeric(lf_n_f[j])
     for (i in seq_len(lf_n_f[j])) {
+      obs_i <- lf_obs_vec[(idx + 1):(idx + nbins)]
       if (removal_switch_f[f] == 0 & lf_n_fi[[j]][i] > 0) {
-        if (lf_switch == 1) { # Multinomial
-          # obs <- obs * lf_n[i] * exp(log_lf_tau[f])
-          # lp[i] <- -RTMB::dmultinom(x = obs, prob = pred, log = TRUE) # REMOVE LATER, if you source this files then reverts to stats::dmultinom which return zero as log like
-          # lp[[j]][i] <- -RTMB::dmultinom(x = lf_obs[[j]][i, ], prob = lf_pred[[j]][i, ], log = TRUE) # REMOVE LATER, if you source this files then reverts to stats::dmultinom which return zero as log like
-          obs_i <- as.numeric(data$lf_obs[[j]][i, ])
-          obs_i <- OBS(obs_i)
+        if (lf_switch == 1) {
           lp[[j]][i] <- -RTMB::dmultinom(x = obs_i, prob = lf_pred[[j]][i, ], log = TRUE)
         }
-        # if (lf_switch == 2) { # Dirichlet
-        #   obs <- obs + 1e-8
-        #   obs <- obs / sum(obs)
-        #   pred <- pred + 1e-8
-        #   pred <- pred / sum(pred)
-        #   lf_alpha <- pred * lf_n[i] * exp(log_lf_tau[f])
-        #   lp[i] <- -ddirichlet(x = obs, alpha = lf_alpha, log = TRUE)
-        # }
-        # if (lf_switch == 3) { # Dirichlet-multinomial
-        #   obs <- obs * lf_n[i]
-        #   lf_alpha <- pred * exp(log_lf_tau[f])
-        #   lp[i] <- -ddirmult(x = obs, size = lf_n[i], alpha = lf_alpha, log = TRUE)
-        # }
-        # if (lf_switch == 9) { # Multinomial offset form
-        #   #   -n_eff * sum(obs * log(pred)) + n_eff * sum(obs * log(obs))
-        #   # Equivalent to n_eff * KL(obs || pred); minimum NLL = 0 when pred == obs.
-        #   # Small constant added to both terms for numerical safety and exact
-        #   # cancellation at perfect fit.
-        #   n_eff <- lf_n[i] * exp(log_lf_tau[f])
-        #   lp[i] <- -n_eff * sum(obs * log(pred + 1e-8))
-        #   lp[i] <- lp[i] + n_eff * sum(obs * log(obs + 1e-8))
       }
+      idx <- idx + nbins
     }
   }
   
   REPORT(lf_pred)
   return(unlist(lp))
 }
+
